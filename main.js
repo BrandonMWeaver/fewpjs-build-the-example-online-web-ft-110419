fb5c18f73e661974de79c9711c07542c596da201
Brandon Weaver <a7xncob@gmail.com>
2020-02-24 20:30:22 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function like(event) {
   mimicServerCall().then(function(response) {
     console.log(response.json());
   }).catch(function(error) {
-    document.querySelector("#modal").class = '';
+    document.querySelector("#modal").className = '';
   });
 }
 
